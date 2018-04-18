6d5265ff7f4678635142b3bed9b760689e357be7
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2018-04-18 10:25:24 -0500

    Irrelevant change to appease github.

--- Public/JS/manager.js
+++ Public/JS/manager.js
@@ -6,12 +6,14 @@ const axios = require('axios');
 var DOW = ['MMM', 'AXP', 'AAPL', 'BA', 'CAT', 'CVX', 'CSCO', 'KO', 'DIS', 'DWDP', 'XOM', 'GE', 'GS', 'HD', 'IBM', 'INTC', 'JNJ', 'JPM', 'MCD', 'MRK', 'MSFT', 'NKE', 'PFE', 'PG', 'TRV', 'UTX', 'UNH', 'VZ', 'V', 'WMT'];
 
 // A forEach loop is run on the DOW array. For each index in the array, the symbol is used in a GET Request from the IEX API, returning the most current price for that stock ('response.data'). The ticker symbol and the response data are then logged to the console.
+
 DOW.forEach(function(symbol) {  
     axios.get(url + symbol + "/price")
     .then(function(response) {
-       console.log("Symbol: " + symbol + ", Price: " + response.data);
+    console.log("Symbol: " + symbol + ", Price: " + response.data);
     })
     .catch(function(error) {
         console.log(error);
     })
 });
+
